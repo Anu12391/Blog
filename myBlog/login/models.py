231f9a512be11c93e6fb6f517a432c2c28aa2618
Anu <anuavr123@gmail.com>
2026-04-20 18:54:29 +0530

    custom user model -----incomplete

--- myBlog/login/models.py
+++ myBlog/login/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+
 from django.db import models
 
+class CustomUserManager(BaseUserManager):
+
+
+
+
+
+
+
 # Create your models here.
+class CustomUser(AbstractBaseUser):
+    GENDER_CHOICES = [('Male', 'Male'), ('Female', 'Female')]
+    first_name = models.CharField(max_length=30,blank=False, null=False)
+    last_name = models.CharField(max_length=60,blank=False, null=False)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    confirm_password = models.CharField(max_length=128)
+    gender = models.CharField(max_length=5, choices=GENDER_CHOICES,default='Male')
+
+    def __str__(self):
+        return self.email
+
+
