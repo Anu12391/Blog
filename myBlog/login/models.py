3786af183ed78f4346fe44c02125de4261540e67
Anu <anuavr123@gmail.com>
2026-04-22 11:30:16 +0530

    created new user manager

--- myBlog/login/models.py
+++ myBlog/login/models.py
@@ -2,7 +2,22 @@ from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 
 from django.db import models
 
-class CustomUserManager(BaseUserManager):
+class NewUserManager(BaseUserManager):
+    def create_user(self,email,password,**extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        new_user=self.model(email=email,**extra_fields)
+        new_user.setpassword(password)
+        new_user.save()
+        return new_user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_admin", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(email, password, **extra_fields)
 
 
 
@@ -11,7 +26,7 @@ class CustomUserManager(BaseUserManager):
 
 
 # Create your models here.
-class CustomUser(AbstractBaseUser):
+class NewUser(AbstractBaseUser):
     GENDER_CHOICES = [('Male', 'Male'), ('Female', 'Female')]
     first_name = models.CharField(max_length=30,blank=False, null=False)
     last_name = models.CharField(max_length=60,blank=False, null=False)
@@ -20,6 +35,16 @@ class CustomUser(AbstractBaseUser):
     confirm_password = models.CharField(max_length=128)
     gender = models.CharField(max_length=5, choices=GENDER_CHOICES,default='Male')
     joining_date = models.DateField(auto_now_add=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    email_verified = models.BooleanField(default=False)
+
+    objects = NewUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+
 
     def __str__(self):
         return self.email
