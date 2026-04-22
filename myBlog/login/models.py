5ff10ee12ace85baaea13ec6e39754f4c10b69b4
Anu <anuavr123@gmail.com>
2026-04-22 13:21:34 +0530

    changed field names in newuser model

--- myBlog/login/models.py
+++ myBlog/login/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 
 from django.db import models
 
@@ -14,8 +15,9 @@ class NewUserManager(BaseUserManager):
         return new_user
 
     def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault("is_admin", True)
+        extra_fields.setdefault("is_active", True)
         extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_staff", True)
 
         return self.create_user(email, password, **extra_fields)
 
@@ -26,17 +28,17 @@ class NewUserManager(BaseUserManager):
 
 
 # Create your models here.
-class NewUser(AbstractBaseUser):
+class NewUser(AbstractBaseUser,PermissionsMixin):
     GENDER_CHOICES = [('Male', 'Male'), ('Female', 'Female')]
     first_name = models.CharField(max_length=30,blank=False, null=False)
     last_name = models.CharField(max_length=60,blank=False, null=False)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
-    confirm_password = models.CharField(max_length=128)
-    gender = models.CharField(max_length=5, choices=GENDER_CHOICES,default='Male')
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES,default='Male')
     joining_date = models.DateField(auto_now_add=True)
-    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
     email_verified = models.BooleanField(default=False)
 
     objects = NewUserManager()
