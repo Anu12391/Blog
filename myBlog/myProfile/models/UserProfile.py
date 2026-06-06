aedcf37914e1e8425ce3e598f18c125ffcb20c9a
Anu <anuavr123@gmail.com>
2026-06-06 19:06:50 +0530

    installed pillow and added profile_pic field to UserProfile model

--- myBlog/myProfile/models/UserProfile.py
+++ myBlog/myProfile/models/UserProfile.py
@@ -7,3 +7,8 @@ class Profile(models.Model):
     user=models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name="profile")
     birthDate=models.DateField(null=True,blank=True)
     about=models.TextField(null=True,blank=True)
+    profile_pic = models.ImageField(
+        upload_to="profile_pics/",
+        null=True,
+        blank=True
+    )
