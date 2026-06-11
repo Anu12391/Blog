911841d6dbad397ffa682b5d0274e44667556759
Anu <anuavr123@gmail.com>
2026-06-11 15:08:28 +0530

    added upload and delete option for pic upload

--- myBlog/static/js/pic_display.js
+++ myBlog/static/js/pic_display.js
@@ -1,16 +1,31 @@
-function previewImage(input,img_id) {
-    const preview = document.getElementById('avatar-preview');
+// Open the hidden file picker
+function triggerFileInput(fieldId) {
+    document.getElementById(fieldId).click();
+}
 
-    // Check if a file was actually selected
+// When a user selects a file to upload
+function handleImageUpload(input, fieldId) {
     if (input.files && input.files[0]) {
+        // 1. Uncheck the clear/delete box if it was previously checked
+        document.getElementById(fieldId + '-clear').checked = false;
+
+        // 2. Generate and update preview image
         const reader = new FileReader();
-
-        // When the file is done reading locally, swap the image src attribute
         reader.onload = function(e) {
-            preview.src = e.target.result;
+            document.getElementById('avatar-preview').src = e.target.result;
         }
-
-        // Read the local file as a temporary Data URL
         reader.readAsDataURL(input.files[0]);
     }
+}
+
+// When a user clicks "Remove Photo"
+function handleImageDeletion(fieldId, defaultUrl) {
+    // 1. Check Django's hidden clear field checkbox
+    document.getElementById(fieldId + '-clear').checked = true;
+
+    // 2. Clear any staged files in the file input file path
+    document.getElementById(fieldId).value = '';
+
+    // 3. Revert preview immediately to the default static avatar
+    document.getElementById('avatar-preview').src = defaultUrl;
 }
