c66eeeaeedc59510b1fcc8512d8686896bb871d9
Anu <anuavr123@gmail.com>
2026-06-12 12:27:57 +0530

    deleted profile pic from media folder on removong pic by user

--- myBlog/myProfile/models/UserProfile.py
+++ myBlog/myProfile/models/UserProfile.py
@@ -1,4 +1,8 @@
+import os
+
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 from myBlog import settings
 
@@ -12,3 +16,25 @@ class Profile(models.Model):
         null=True,
         blank=True
     )
+
+
+@receiver(pre_save, sender=Profile)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """
+    Deletes old file from filesystem when corresponding object is updated
+    with a new file or cleared.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = sender.objects.get(pk=instance.pk).profile_pic
+    except sender.DoesNotExist:
+        return False
+
+    new_file = instance.profile_pic
+
+    # If the file has changed, or been cleared (new_file is empty/False)
+    if not old_file == new_file:
+        if old_file and os.path.isfile(old_file.path):
+            os.remove(old_file.path)
