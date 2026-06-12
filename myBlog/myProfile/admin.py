dcf9995b7029ed6134e4ea965f82b02135e78ce9
Anu <anuavr123@gmail.com>
2026-06-12 16:24:10 +0530

    added topic in admin pane--customised

--- myBlog/myProfile/admin.py
+++ myBlog/myProfile/admin.py
@@ -3,6 +3,14 @@ from django.contrib import admin
 from myProfile.models.TopicsOfInterest import Topics
 from myProfile.models.UserProfile import Profile
 
+
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('topicId','topicName', 'topicDescription', )
+
+    ordering = ('topicId',)
+
+
 # Register your models here.
 admin.site.register(Profile)
-admin  .site.register(Topics)
+admin  .site.register(Topics,TopicAdmin)
--- myBlog/myProfile/models/TopicsOfInterest.py
+++ myBlog/myProfile/models/TopicsOfInterest.py
@@ -5,3 +5,6 @@ class Topics(models.Model):
     topicId = models.AutoField(primary_key=True)
     topicName = models.CharField(max_length=100)
     topicDescription = models.CharField(max_length=500)
+
+
+
