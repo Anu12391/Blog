05a8bab144097c5c01fbda38bb8a30febdaa73e2
Anu <anuavr123@gmail.com>
2026-06-12 13:16:11 +0530

    topics model added

--- myBlog/myProfile/admin.py
+++ myBlog/myProfile/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
+from myProfile.models.TopicsOfInterest import Topics
 from myProfile.models.UserProfile import Profile
 
 # Register your models here.
 admin.site.register(Profile)
+admin  .site.register(Topics)
---
+++ myBlog/myProfile/models/TopicsOfInterest.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Topics(models.Model):
+    topicId = models.AutoField(primary_key=True)
+    topicName = models.CharField(max_length=100)
+    topicDescription = models.CharField(max_length=500)
