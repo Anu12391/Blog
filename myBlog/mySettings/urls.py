d956f34307868a606d6710c0b93fafc00aa8cd8e
Anu <anuavr123@gmail.com>
2026-06-19 18:13:20 +0530

    cbv for search---done

--- myBlog/mySettings/urls.py
+++ myBlog/mySettings/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from common.Constants.ApplicationNames import AppNames
 from common.Constants.VariableNames import AuthUrls
+from mySettings.views.TopicSearch import TopicSearchView
 from mySettings.views.TopicSelectionView import TopicSelection
 
 
 app_name = AppNames.MySettings.app_name
 
 urlpatterns = [
-    path(AuthUrls.MyTopics.myTopics_subUrl,TopicSelection.as_view(),name=AuthUrls.MyTopics.myTopics_reverseName)
+    path(AuthUrls.MyTopics.myTopics_subUrl,TopicSelection.as_view(),name=AuthUrls.MyTopics.myTopics_reverseName),
+    path('topics/', TopicSearchView.as_view(), name='topic_search_list'),
 ]
