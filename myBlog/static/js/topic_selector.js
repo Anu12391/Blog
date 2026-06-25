49f3e6e73ea8964a589404bc90b5564c7d15e417
Anu <anuavr123@gmail.com>
2026-06-25 16:32:12 +0530

    search query not picking value when search button clicked---issue fixed

--- myBlog/static/js/topic_selector.js
+++ myBlog/static/js/topic_selector.js
@@ -5,5 +5,29 @@ function toggleTopic(element) {
     // 2. Toggle Bootstrap classes to swap the colors instantly
     element.classList.toggle('bg-danger');
     element.classList.toggle('text-white');
-    element.classList.toggle('text-danger'); // Removes the initial red text color
+    element.classList.toggle('text-danger');
+     // Removes the initial red text color
 }
+
+
+
+function fetchSearchedTopic(){
+console.log("JS LOADED");
+    document.getElementById('button-search').addEventListener('click',async () => {
+
+     console.log("BUTTON:", document.getElementById('button-search'));
+
+    const searchQuery = document.getElementById('search-input').value
+    console.log(searchQuery)
+
+    console.log(searchQuery.value);
+    const response = await fetch(`${myTopicsUrl}?query=${encodeURIComponent(searchQuery)}`);
+    const data = await response.json();
+
+    console.log(data);
+//update Ui here
+    }
+    )
+
+    }
+  document.addEventListener('DOMContentLoaded', fetchSearchedTopic);
