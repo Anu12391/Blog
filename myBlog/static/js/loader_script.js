cd2a5bc6d7849b4cfca53e8ca29cdf95d389338d
Anu <anuavr123@gmail.com>
2026-07-18 16:06:04 +0530

    loader code cleanup

--- myBlog/static/js/loader_script.js
+++ myBlog/static/js/loader_script.js
@@ -1,6 +1,4 @@
-/**
- * Global UI Loader Controller
- */
+
 const loader = {
     element: null,
 
@@ -16,8 +14,7 @@ const loader = {
     show() {
         const el = this.getElement();
         if (el) {
-            // Remove the absolute hiding style if it was applied previously
-            el.style.removeProperty("display");
+
             el.classList.add("show");
         } else {
             console.warn("Target element '#global-loader' not found to show.");
@@ -28,11 +25,10 @@ const loader = {
     hide() {
         const el = this.getElement();
         if (el) {
-            // 1. Remove the active visibility class
+
             el.classList.remove("show");
 
-//            // 2. Heavy fallback override: forces it away even if custom CSS rules collide
-//            el.style.setProperty("display", "none", "important");
+
         } else {
             console.warn("Target element '#global-loader' not found to hide.");
         }
