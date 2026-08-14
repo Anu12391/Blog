5c3e37410cea0d50dcf8818627ef24a01dd26fa4
Anu <anuavr123@gmail.com>
2026-08-14 15:57:10 +0530

    added success url after posting post

--- myBlog/myPosts/views/CreatePostView.py
+++ myBlog/myPosts/views/CreatePostView.py
@@ -1,5 +1,7 @@
+from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from common.Constants.VariableNames import AuthUrls
 from myPosts.forms.PostForm import PostForm
 from myPosts.models.CreatePostModel import Post
 
@@ -8,10 +10,13 @@ class CreatePostView(CreateView):
     form_class = PostForm
     model = Post
     template_name = 'myPosts/create_post.html'
+    success_url = reverse_lazy(AuthUrls.Dashboard.dashboard_redirectName)
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
         return super().form_valid(form)
+
+
 
 
 
