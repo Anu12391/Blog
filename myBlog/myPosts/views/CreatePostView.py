2485db9b39ca06407930047d1f73a39ab98ebd76
Anu <anuavr123@gmail.com>
2026-08-21 19:44:28 +0530

    added login check for mypost

--- myBlog/myPosts/views/CreatePostView.py
+++ myBlog/myPosts/views/CreatePostView.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -6,7 +7,7 @@ from myPosts.forms.PostForm import PostForm
 from myPosts.models.CreatePostModel import Post
 
 
-class CreatePostView(CreateView):
+class CreatePostView(LoginRequiredMixin,CreateView):
     form_class = PostForm
     model = Post
     template_name = 'myPosts/create_post.html'
